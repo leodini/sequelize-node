46a4e51f428a71aa5f3cf24552d80d90f6ccd5af
leonardo dini <leonardodini@pop-os.localdomain>
2020-09-29 01:11:26 -0300

    model and controller for user

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 const express = require("express")
+const UserController = require("./controllers/UserController")
 
 const routes = express.Router()
 
-routes.get('/', (_req, res) => {
-    return res.json({ msg: 'hello world'})
-})
+routes.post('/users', UserController.store)
+routes.get('/users', UserController.index)
 
 module.exports = routes
